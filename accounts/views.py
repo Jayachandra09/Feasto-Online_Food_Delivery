a552d06d286ec9db008999e430f32427ed93bcc2
Jayachandra <jayachandraburla2244@gmail.com>
2025-03-23 20:26:45 +0530

    Vendor and Customer dashboard edits

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import default_token_generator
 
 from .forms import UserForm
 from .models import User, UserProfiles
@@ -9,9 +11,7 @@ from .utils import detectuser, send_verification_email
 from django.contrib.auth.decorators import login_required, user_passes_test
 
 from django.core.exceptions import PermissionDenied
-
-from django.utils.http import urlsafe_base64_decode
-from django.contrib.auth.tokens import default_token_generator
+from vendor.models import Vendor
 
 
 # Restrict the Vendor from accessing the Customer page
