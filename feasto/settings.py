4983fafe8d14432185d34153d1ac62780857aca7
Jayachandra <jayachandraburla2244@gmail.com>
2025-03-25 15:18:58 +0530

    Restaurant Profile and Google Geocoding

--- feasto/settings.py
+++ feasto/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
                 "accounts.context_processors.get_vendor",
+                "accounts.context_processors.get_google_api",
             ],
         },
     },
@@ -154,3 +155,5 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Feasto Food <feasto.jay@gmail.com>'
+
+GOOGLE_API_KEY = 'AIzaSyB49M14nxj0nBFqoWLqNrcwSYaixedalSY'
